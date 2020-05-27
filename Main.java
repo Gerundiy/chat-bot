2aeaccdbe08ea462e87498228c3b39aabbc7b5a5
Kpc <kiriill.novikov@gmail.com>
2020-05-27 15:50:36 +0300

    chatbot v1

---
+++ Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	System.out.println("Hello! My name is Fortiss");
+	System.out.println("I was created in 2020");
+    }
+}
